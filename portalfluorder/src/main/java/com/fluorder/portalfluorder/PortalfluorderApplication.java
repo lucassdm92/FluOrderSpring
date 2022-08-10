3e3e68ecdcacee663cd99562623a715b24ef4f5b
Lucas <Lucas.sdm92@gmail.com>
2022-08-10 01:34:10 +0100

    frist commit

---
+++ portalfluorder/src/main/java/com/fluorder/portalfluorder/PortalfluorderApplication.java
@@ -0,0 +1,13 @@
+package com.fluorder.portalfluorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalfluorderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortalfluorderApplication.class, args);
+	}
+
+}
---
+++ portalfluorder/src/test/java/com/fluorder/portalfluorder/PortalfluorderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fluorder.portalfluorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortalfluorderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
